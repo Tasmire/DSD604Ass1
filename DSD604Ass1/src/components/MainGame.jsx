c39fc0bcab03d561951004a2114601ac84f75328
Bella Fickling <igfickling@gmail.com>
2025-06-24 16:26:28 +1200

    Visual changes

--- DSD604Ass1/src/components/MainGame.jsx
+++ DSD604Ass1/src/components/MainGame.jsx
@@ -3,6 +3,7 @@ import { maoriPlaceNamesData, sortedListAnswers } from '../assets/maoriPlaceName
 import { Random } from '../utilities/Random';
 import StoreAnswer from './StoreAnswer';
 import HintButton from './HintButton';
+import Swal from 'sweetalert2';
 
 import Select from 'react-select';
 import { selectCustomStyles } from '../utilities/SelectReactSetting';
@@ -20,7 +21,7 @@ export default function MainGame() {
     });
     const answerData = sortedListAnswers();
     const [answer, setAnswer] = useState("");
-    const [winlose, setWinLose] = useState("");
+    //const [winlose, setWinLose] = useState("");
     const [answers, setAnswers] = useState([]);
     const [isAnsweredCorrectly, setIsAnsweredCorrectly] = useState(false);
 
@@ -29,7 +30,7 @@ export default function MainGame() {
 
     const onClickHandlerNewGame = () => {
         setAnswer("");
-        setWinLose("");
+        //setWinLose("");
         let rand = Random(allData.length);
         setGameData({
             Placename: allData[rand].Placename,
@@ -58,27 +59,43 @@ export default function MainGame() {
                 isCorrect,
             }
         ]);
-        setWinLose("- you " + winLoseCalc(answerLet));
+
+        //setWinLose(" - you " + winLoseCalc(answerLet));
+
         if (isCorrect) {
+            Swal.fire({
+                title: 'Correct!',
+                text: `You guessed correctly! The place name is ${gameData.Placename}. Generate a new place name to play again.`,
+                confirmButtonText: 'OK',
+                theme: 'dark'
+            });
             setIsAnsweredCorrectly(true);
+        } else {
+            Swal.fire({
+                title: 'Incorrect!',
+                text: `Sorry, try again!`,
+                confirmButtonText: 'OK',
+                theme: 'dark'
+            });
         }
         console.log("answer = ", answerLet + " gameplay = ", gameData.Placename + " gameDataLet.Placename = ", gameDataLet.Placename);
     };
 
-    const winLoseCalc = (answerLet) => {
-        if (answerLet !== undefined && answerLet !== null) {
-            if (answerLet === gameData.Placename) {
-                return "win";
-            } else {
-                return "lose";
-            }
-        }
-        return "";
-    };
+    // const winLoseCalc = (answerLet) => {
+    //     if (answerLet !== undefined && answerLet !== null) {
+    //         if (answerLet === gameData.Placename) {
+    //             return "win! Click 'Generate a new place name' to play again.";
+    //         } else {
+    //             return "lose. Try again!";
+    //         }
+    //     }
+    //     return "";
+    // };
 
     return (
         <div className="main-game container-fluid py-5">
             <div className="row justify-content-center">
+                <h1 className="text-center mb-4 display-3">Guess the Māori Place Name</h1>
                 <div className="col-12 col-md-10 col-lg-12">
                     <div className="card shadow">
                         <div className="card-body">
@@ -86,19 +103,24 @@ export default function MainGame() {
                                 className="btn btn-primary mb-4 w-100 display-4"
                                 onClick={onClickHandlerNewGame}
                             >
-                                Generate a random place name
+                                Generate a new place name
                             </button>
                             <div className="mb-4 text-center">
                                 <h2 className="display-4">{gameData.Meaning}</h2>
-                                <h4 className="fs-3 mt-3">
-                                    {answer ? "You selected " + answer + winlose : ""}
-                                </h4>
                             </div>
                             <div>
                                 <Select
                                     styles={selectCustomStyles}
                                     options={answerData}
                                     className="selectDropDownStyle"
+                                    theme={theme => ({
+                                        ...theme,
+                                        colors: {
+                                            ...theme.colors,
+                                            primary: '#007bff',
+                                            neutral0: '#333',
+                                            neutral20: '#555',
+                                        }})}
                                     value={answerData.find((opt) => opt.value === answer) || null}
                                     onChange={handleAnswerChange}
                                     placeholder={answer !== "" ? answer : "Select an Answer"}
--- DSD604Ass1/src/utilities/SelectReactSetting.jsx
+++ DSD604Ass1/src/utilities/SelectReactSetting.jsx
@@ -1,9 +1,18 @@
 export const selectCustomStyles = {
     option: (provided, state) => ({
         ...provided,
-        borderBottom: "1px solid green",
-        color: state.isSelected ? "yellow" : "black",
-        backgroundColor: state.isSelected ? "green" : "white",
+        borderBottom: "1px solid grey",
+        color: state.isSelected ? "white" : "white",
+        backgroundColor: state.isSelected ? "green" : "#333",
         padding: "0px",
+    }),
+    control: (provided, state) => ({
+        ...provided,
+        backgroundColor: state.isDisabled ? "#0a270b" : provided.backgroundColor,
+        borderColor: state.isDisabled ? "#4caf50" : provided.borderColor,
+    }),
+    placeholder: (provided, state) => ({
+        ...provided,
+        color: state.isDisabled ? "#aaa" : "#fff",
     }),
 };
